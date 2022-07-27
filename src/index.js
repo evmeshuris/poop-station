e5cb3880fe6bee4797ff1565d597859ec03e5cf6
evmeshuris <101578340+evmeshuris@users.noreply.github.com>
2022-07-27 10:40:59 -0700

    add styles to images; fix GetInfo button

--- src/index.js
+++ src/index.js
@@ -2,26 +2,52 @@ import "bootstrap";
 import "bootstrap/dist/css/bootstrap.min.css";
 import "./css/styles.css";
 import $ from "jquery";
-import LitterInfo from "./poop-station.js";
-import { ApiCall, ErrorHandling } from "./utility.js";
+// import LitterInfo from "./poop-station";
 
-$(document).ready(function () {
-  $("#showCount").click(function (e) {
-    e.preventDefault();
-    const cycle_count = $("#showCount").val();
-    LitterInfo.getInfo(cycle_count).then(function (newResponse) {
-      showCount(newResponse, cycle_count);
-    });
+$(document).ready(function(){
+  
+  // var settings = {
+  //   "url": "https://litterbot.api.theemm.com/get_info",
+  //   "method": "GET",
+  //   "timeout": 0,
+  //   "headers": {
+  //     "X-API-Key": "0XqYjqekJL60tVjNWbkvG9PXgcxjtEMpxWeLQ3ba"
+  //   },
+  // };
+  
+  // $.ajax(settings).done(function (response) {
+  //   alert(response);
+  // });
+
+  $('#get-info').click(function() {
+    const myArr = JSON.parse(getInfo());
+    alert(myArr.name);
+    document.getElementById("show-name").innerHTML = "Name of the toilet: " + myArr.name;
+    document.getElementById("show-serial").innerHTML = "Serial number: " + myArr.serial;
+    document.getElementById("show-count").innerHTML = "How many times cats did their business: " + myArr.cycle_count;
+    document.getElementById("show-drawer").innerHTML = "Is toilet needs to get clean: " + myArr.is_waste_drawer_full;
+    document.getElementById("show-level").innerHTML = "Drawer level: " + myArr.waste_drawer_level;
   });
+
+  // $('#clean').click(function() {
+  //   const myArr = JSON.parse(getInfo());
+  // });
+
+  function getInfo()  {
+    var response =  `{
+    "name": "Kakahina Station",
+    "serial": "LR3CI451198",
+    "cycle_capacity": 33,
+    "cycle_count": 9,
+    "cycles_after_drawer_full": 0,
+    "is_waste_drawer_full": false,
+    "waste_drawer_level": 27.3
+  }`;
+    return response;
+  }
 });
 
-function showCount(cycle_count) {
-  if (response.result === "success") {
-    $("#showCount").text(
-      `How many time cats did their business ${cycle_count}`
-    );
-  } else {
-    $(".showErrors").text(`${LitterInfo.error}`);
-  }
-}
+
+
+
 
--- src/poop-station.js
+++ src/poop-station.js
@@ -1,75 +1,20 @@
-import { ApiCall, ErrorHandling } from "./utility.js";
+//import { ApiCall, ErrorHandling } from "./utility.js";
+var request = require('request');
 
-export class LitterInfo {
-  static async getLitterInfo() {
-    // let url = `https://litterbot.api.theemm.com/get_info`;
-    let url = `https://litterbot.api.theemm.com/get_info/key=${process.env.API_KEY_BING}`;
-    const response = await ApiCall.getApi(url);
-    const isError = ErrorHandling.handleError(response);
-    if (!isError) {
-      const cycle_count = response.cycle_count;
-      const cycleCount = cycle_count.map(({ cycle_count, name }) => ({ cycle_count, name }));
-      return cycleCount;
-    }
-    return true;
+
+export default class LitterInfo {
+  static async getInfo() {
+    var options = {
+      'method': 'GET',
+      'url': 'https://litterbot.api.theemm.com/get_info',
+      'headers': {
+        'X-API-Key': '0XqYjqekJL60tVjNWbkvG9PXgcxjtEMpxWeLQ3ba'
+      }
+    };
+    request(options, function (error, response) {
+      if (error) throw new Error(error);
+      console.log(response.body);
+    });
+
   }
 }
-
-// export default class LitterInfo {
-//   static async getInfo() {
-//     try {
-//       const response = await fetch(
-//         `https://litterbot.api.theemm.com/get_info`
-//         // "url": "https://litterbot.api.theemm.com/get_info",
-//         // "method": "GET",
-//         // "timeout": 0,
-//         // "headers": {
-//         // "X-API-Key": "0XqYjqekJL60tVjNWbkvG9PXgcxjtEMpxWeLQ3ba"
-//       );
-//       if (!response.ok) {
-//         throw Error(response.statusText);
-//       }
-//       return await response.json();
-//     } catch (error) {
-//       return Error(error);
-//     }
-//   }
-// }
-
-// var settings = {
-//   "url": "https://litterbot.api.theemm.com/get_info",
-//   "method": "GET",
-//   "timeout": 0,
-//   "headers": {
-//     "X-API-Key": "0XqYjqekJL60tVjNWbkvG9PXgcxjtEMpxWeLQ3ba"
-//   },
-// };
-
-// $.ajax(settings).done(function (response) {
-//   console.log(response);
-// });
-
-// $.ajax({
-//   url: "https://litterbot.api.theemm.com/get_info",
-//   data: { signature: authHeader },
-//   type: "GET",
-//   beforeSend: function(xhr){xhr.setRequestHeader('X-Test-Header', 'test-value');},
-//   success: function() { alert('Success!' + authHeader); }
-// });
-
-// $.ajax({
-//   url: URL,
-//   type: 'GET',
-//   dataType: 'json',
-//   headers: {
-//       'header1': 'value1',
-//       'header2': 'value2'
-//   },
-//   contentType: 'application/json; charset=utf-8',
-//   success: function (result) {
-//      // CallBack(result);
-//   },
-//   error: function (error) {
-      
-//   }
-// });
